5615c005ddd713eaee48a507d2b3054042d31bdb
ananyaseelam <60414577+ananyaseelam@users.noreply.github.com>
2020-08-28 22:15:25 -0400

    updating navigation

--- frontend/App.js
+++ frontend/App.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { mapping, light as lightTheme } from '@eva-design/eva'
-import { ApplicationProvider, Layout, Text } from '@ui-kitten/components'
+import { ApplicationProvider, Layout, Text} from '@ui-kitten/components'
+import { IconRegistry } from 'react-native-ui-kitten'
 import TabNavigator from './src/navigation/TabNavigator'
 import { Asset, Font, Icon } from 'expo';
-
+import { EvaIconsPack } from '@ui-kitten/eva-icons'
 const ApplicationContent = () => (
   <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
     <Text>Welcome to UI Kitten</Text>
@@ -11,9 +12,12 @@ const ApplicationContent = () => (
 )
 
 const App = () => (
+  <Fragment>
+    <IconRegistry icons={EvaIconsPack} />
     <ApplicationProvider mapping={mapping} theme={lightTheme}>
         <TabNavigator />
     </ApplicationProvider>
+    </Fragment>
 )
 
 export default App
--- frontend/src/navigation/TabNavigator.js
+++ frontend/src/navigation/TabNavigator.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createAppContainer } from 'react-navigation'
 import { createBottomTabNavigator } from 'react-navigation-tabs'
-import { Icon } from '@ui-kitten/components'
+import { Icon } from 'react-native-ui-kitten'
 import { FeedNavigator } from './StackNavigator'
 import MapView from '../screens/MapView'
 import AddPost from '../screens/AddPost'
@@ -9,20 +9,59 @@ import Home from '../screens/Home'
 import Profile from '../screens/Profile'
 
 const TabNavigator = createBottomTabNavigator({
-    Home: {
-        screen: Home
-    },
+
     Feed: {
-        screen: FeedNavigator
+        screen: FeedNavigator,
+        navigationOptions: {
+            tabBarIcon: ({ focused }) => (
+              <Icon
+                name='home-outline'
+                width={32}
+                height={32}
+                fill={focused ? '#111' : '#939393'}
+              />
+            )
+          }
+
     },
     AddPost: {
-        screen: AddPost
+        screen: AddPost,
+      navigationOptions: {
+        tabBarIcon: ({ focused }) => (
+          <Icon
+            name='plus-square-outline'
+            width={32}
+            height={32}
+            fill={focused ? '#111' : '#939393'}
+          />
+        )
+      }
     },
     MapView: {
-        screen: MapView
+        screen: MapView,
+        navigationOptions: {
+            tabBarIcon: ({ focused }) => (
+              <Icon
+                name='map-outline'
+                width={32}
+                height={32}
+                fill={focused ? '#111' : '#939393'}
+              />
+            )
+          }
     },
     Profile: {
-        screen: Profile
+        screen: Profile,
+        navigationOptions: {
+            tabBarIcon: ({ focused }) => (
+              <Icon
+                name='person-outline'
+                width={32}
+                height={32}
+                fill={focused ? '#111' : '#939393'}
+              />
+            )
+          }
     }
 })
   
