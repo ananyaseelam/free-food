8d00a46eb0da19dc8dba945276595ccbea34bd26
ananyaseelam <60414577+ananyaseelam@users.noreply.github.com>
2020-08-28 22:02:22 -0400

    Update AddPost.js

--- frontend/src/screens/AddPost.js
+++ frontend/src/screens/AddPost.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, TextInput, Text, ScrollView, Button} from 'react-native';
+import { Input} from '@ui-kitten/components';
+import { ApplicationProvider, Layout, IconRegistry } from 'react-native-ui-kitten'
+import { mapping, light as lightTheme } from '@eva-design/eva'
 
 export default class AddPost extends Component {
   constructor(props) {
@@ -9,6 +12,7 @@ export default class AddPost extends Component {
       food_name: '',
       location:'',
       description:'',
+      avatarSource: '',
     };
   }
 
@@ -25,15 +29,31 @@ export default class AddPost extends Component {
   }
 
   handleSubmit = () => {
-    //needs to send data to backend
+    this.sendPost()
   }
 
-
-
+  sendPost = () => {
+    const obj = {'food': this.state.food_name, 'location': this.state.location, 
+              'description': this.state.description};
+    const blob = new Blob([JSON.stringify(obj, null, 2)], {type : 'application/json'});
+    let postData = {
+        method: 'POST',
+        mode: 'no-cors', 
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: blob
+    }
+    fetch('', postData)
+  };
+ 
   render(){
     return(
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Add Post Screen
+        <Text style={styles.HeaderText}>Add Posting Here: </Text>
+        <Text style= {styles.baseText}>
+        
         {"\n"}
         Food: {this.state.food_name}
         {"\n"}
@@ -41,29 +61,27 @@ export default class AddPost extends Component {
         {"\n"}
         Description: {this.state.description}
         </Text>
-
-
         <TextInput
-          style={{ height: 40, borderColor: 'gray', borderWidth: 1, }}
+          style={styles.input}
           value = {this.state.food_name}
-          placeholder="     Food Name      "
+          placeholder=" Food Name "
           onChangeText={this.handleFoodChange}
         />
         <TextInput
-          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          style={styles.input}
           value = {this.state.location}
-          placeholder="     Location     "
+          placeholder=" Pickup Location "
           onChangeText={this.handleLocationChange}
         />
         <TextInput
-          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          style={styles.input}
           value = {this.state.description}
-          placeholder="     Description     "
+          placeholder=" Description "
           onChangeText={this.handleDescriptionChange}
         />
 
         <Button
-          title="Submit"
+          title="Create Post"
           onSubmit={() => postData()//send data to backend
         }
         />
@@ -71,3 +89,41 @@ export default class AddPost extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    //backgroundColor: '#FF6347',
+    margin: 30,
+    alignItems: 'center',
+    //justifyContent: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'black',
+    minWidth: 250,
+    marginTop: 10,
+    marginBottom:10,
+    marginHorizontal: 20,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 3,
+  },
+  HeaderText:{
+    fontSize: 22,
+    textAlign: 'center',
+    color: 'black',
+    fontFamily: 'System',
+    fontWeight: "bold",
+    justifyContent:'flex-start',
+    margin: 5,
+  },
+  baseText:{
+    fontSize: 18,
+    textAlign: 'center',
+    color: 'black',
+    fontFamily: 'System',
+    fontWeight: "bold",
+    justifyContent:'space-evenly'
+  }
+});
